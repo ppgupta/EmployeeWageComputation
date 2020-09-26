960585151f8dd852ce39307c6f5a5c4322b75d3a
ppgupta <premprakashnayak69@gmail.com>
2020-09-26 14:20:11 +0530

    [premprakash]Add part time employee and wage using switch case statement

--- EmployeeWageBuilderUC1.java
+++ EmployeeWageBuilderUC1.java
@@ -1,23 +1,27 @@
 package javapractice;
 
-public class EmployeeWageBuilderUC3 {
-	public static void main(String[] args) {
-		 int IS_PART_TIME=1;
+public class EmployeeWageBuilderUC4 {
+	public static final  int IS_PART_TIME=1;
+	public static final  int IS_FULL_TIME=2;
+	public static final   int EMP_RATE_PER_HOUR=20;
 
-		 int IS_FULL_TIME=2;
-	     int EMP_RATE_PER_HOUR=20;
+	public static void main(String[] args) {
+	     
 	     int empHrs=0;
 	     int empWage=0;
-	 	double empCheck=Math.floor(Math.random()*10)%3;
-	   if(empCheck==IS_PART_TIME) {
+	 	int empCheck=(int )Math.floor(Math.random()*10)%3;
+	   switch(empCheck) {
+	   case  IS_PART_TIME:
 		   empHrs=4;
+		   break;
+	   case  IS_FULL_TIME:
+		   empHrs=8;
+		   break;
+	   default:
+		   empHrs=0;
 		   
 	   }
-	   else if(empCheck==IS_FULL_TIME) {
-		   empHrs=8;
-	   }
-	   else {
-		   empHrs=0;}
+	   
 	   empWage=empHrs*EMP_RATE_PER_HOUR;
 		System.out.print("Employee wage is "+ empWage);
 
